67541731b2015f1ac6acabdb0c5dbd6535db5cde
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-06-05 14:27:55 +1000

    chore: revert ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@pinia/nuxt", "@sidebase/nuxt-auth"],
-  extends: ["@nuxt/ui-pro"],
+  // extends: ["@nuxt/ui-pro"],
   auth: {
     provider: {
       type: "authjs",
