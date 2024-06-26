7ee9e9ffc9d7c979006f67e460fd390818242bac
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-06-26 21:34:44 +1000

    update key

--- server/aws/cloudwatch.ts
+++ server/aws/cloudwatch.ts
@@ -7,7 +7,7 @@ const client = new CloudWatchLogsClient({
   region: process.env.AWS_REGION, // "ap-southeast-2", // Replace with your AWS region
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
