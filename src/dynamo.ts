781f79d0d4fa17f09fcca10cf32c426a766938db
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-06-26 21:33:39 +1000

    fix key

--- src/dynamo.ts
+++ src/dynamo.ts
@@ -14,7 +14,7 @@ const dynamo = new DynamoDB({
   region: process.env.AWS_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
